1f7aee986ec06677a9f0c26ebb902a83b435e7f7
salana <78548678+hhhh514@users.noreply.github.com>
2026-08-11 19:14:41 +0800

    Delete DB directory

--- DB/02_sample_data.sql
+++
@@ -1,2 +0,0 @@
-USE work;
-INSERT INTO users(phone_number,user_name,email,password_hash,biography) VALUES ('0912345678','Demo User','demo@example.com','$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy','Hello!');
